b94acc5dc94ea17b7320f2228bba5a14ce7f5302
sbaru <sbaru@lantronix.com>
2017-09-29 14:56:05 +0530

    fixing aes error and add public and private keys

--- aes.js
+++ aes.js
@@ -2,12 +2,11 @@
 const crypto = require('crypto');
 const fs = require('fs');
 let cipher = crypto.createCipher('aes192', '77cb54b4-6f08-429d-a558-add6cc8599e8');
-let decipher = crypto.createDecipher('aes192', 'b492ca78-6a29-4686-a95f-e7aad40ded80');
+let decipher = crypto.createDecipher('aes192', '77cb54b4-6f08-429d-a558-add6cc8599e8');
 
-plain text
 let encrypted = cipher.update('this is plain string to encrypt', 'utf8', 'hex');
 encrypted += cipher.final('hex');
-let plain = decipher.update("", 'hex', 'utf8')
+let plain = decipher.update(encrypted, 'hex', 'utf8')
 plain += decipher.final('utf8');
 console.log(encrypted)
 console.log('the plain', plain);
@@ -49,6 +48,6 @@ var decryptStringWithRsaPrivateKey = function(toDecrypt, relativeOrAbsolutePatht
 
 // encrypted = encryptStringWithRsaPublicKey('4217b732-47ca-469a-a24e-e626cce0eaa4');
 // console.log(encrypted);
-
-let decrypt = decryptStringWithRsaPrivateKey('qP5YETKSSlOjkFre0qGoE6Ux6oZOkH49fEEeKXrXHwO3prLlyrS/LJVRdAAUCIIfq+YPPZ22RhuNwjqVWFVGUH1N9dqQFLVCe8NL169q0V/bJ+ghckwiEzdM7GXwUPBFD7nNFkt4rw+Du+8tb21Vn3/uZD3ngV0Tr0clJSHX9lFAL7w8X7xUNA9Y35h1/oK7iACXfEOhUQke8PTAO6mvXlph9uFTplPZ5PvWTGLQMzYUJtJw5pMeN4O+WEp4spldYVbJHd2ngHCdixqdGTpfhH3V9AV33O2M2XjkwfXtLF2JdawAJE+BRJspHkoPR9ztZjG0tS4rFJ8BA/u5Ej5H/A==');
-console.log(decrypt);
+//
+// let decrypt = decryptStringWithRsaPrivateKey('qP5YETKSSlOjkFre0qGoE6Ux6oZOkH49fEEeKXrXHwO3prLlyrS/LJVRdAAUCIIfq+YPPZ22RhuNwjqVWFVGUH1N9dqQFLVCe8NL169q0V/bJ+ghckwiEzdM7GXwUPBFD7nNFkt4rw+Du+8tb21Vn3/uZD3ngV0Tr0clJSHX9lFAL7w8X7xUNA9Y35h1/oK7iACXfEOhUQke8PTAO6mvXlph9uFTplPZ5PvWTGLQMzYUJtJw5pMeN4O+WEp4spldYVbJHd2ngHCdixqdGTpfhH3V9AV33O2M2XjkwfXtLF2JdawAJE+BRJspHkoPR9ztZjG0tS4rFJ8BA/u5Ej5H/A==');
+// console.log(decrypt);
